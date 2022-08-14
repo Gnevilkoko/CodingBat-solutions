00bd483aab8845c5c9c8a8b4745d7b2c3ff2b464
Gnevilkoko <111231899+Gnevilkoko@users.noreply.github.com>
2022-08-14 16:17:30 +0300

    Create stringE.java

---
+++ java/Warmup-1/stringE.java
@@ -0,0 +1,7 @@
+public boolean stringE(String str) {
+   int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == 'e') count++;
+        }
+        return count >= 1 && count <= 3;
+}
