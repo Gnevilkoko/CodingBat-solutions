2d2dbf62c8022ad72c42af5e9088dccf29c3ce7f
Gnevilkoko <111231899+Gnevilkoko@users.noreply.github.com>
2022-08-16 16:12:35 +0300

    Delete java/Logic-2/int maxBig = goal directory
    Signed-off-by: Gnevilkoko <111231899+Gnevilkoko@users.noreply.github.com>

--- java/Logic-2/int maxBig = goal/makeBricks.java
+++
@@ -1,8 +0,0 @@
-public boolean makeBricks(int small, int big, int goal) {
-   int maxBig = goal/5;
-
-        if(big > maxBig) goal -= maxBig*5;
-        else goal -= big*5;
-
-        return goal <= small;
-}
