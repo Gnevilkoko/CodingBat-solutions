3507836a68e0883347ddb8ab6670eec05001d142
Gnevilkoko <111231899+Gnevilkoko@users.noreply.github.com>
2022-08-16 03:26:25 +0300

    Create topping2.java
    Signed-off-by: Gnevilkoko <111231899+Gnevilkoko@users.noreply.github.com>

---
+++ java/Map-1/topping2.java
@@ -0,0 +1,5 @@
+public Map<String, String> topping2(Map<String, String> map) {
+  if(map.containsKey("ice cream")) map.put("yogurt", map.get("ice cream"));
+  if(map.containsKey("spinach")) map.put("spinach", "nuts");
+  return map;
+}
