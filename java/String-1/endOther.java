cc8d3b0b202d43af3781702d1d01eef1879574a1
Gnevilkoko <111231899+Gnevilkoko@users.noreply.github.com>
2022-09-09 20:21:49 +0300

    Delete endOther.java
    Signed-off-by: Gnevilkoko <111231899+Gnevilkoko@users.noreply.github.com>

--- java/String-1/endOther.java
+++
@@ -1,8 +0,0 @@
-public boolean endOther(String a, String b) {
-  if(a.length() < b.length()){
-    String temp = a;
-    a = b;
-    b = temp;
-  }
-  return a.substring(a.length()-b.length()).equalsIgnoreCase(b);
-}
